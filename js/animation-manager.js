90e744b00d114dc14191767fa9248e3879cf3da9
Asia <dobrowolska.krakow@gmail.com>
2024-09-11 17:37:33 +0200

    manageg section work

---
+++ js/animation-manager.js
@@ -0,0 +1,26 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const investorSection = document.querySelector('#manager');
+  const investorList = document.querySelector('.manager__list');
+  const headerHeight = 110;
+  const margin = 150;
+
+  function handleScroll() {
+    const sectionTop = investorSection.getBoundingClientRect().top;
+    const sectionBottom = investorSection.getBoundingClientRect().bottom;
+    const viewportHeight = window.innerHeight;
+
+    if (sectionTop <= headerHeight && sectionBottom > headerHeight) {
+      investorList.classList.add('visible');
+    }
+
+    if (sectionBottom < 0 + margin || sectionTop > viewportHeight - margin) {
+      investorList.classList.remove('visible');
+    }
+  }
+
+  window.addEventListener('scroll', handleScroll);
+
+  handleScroll();
+});
