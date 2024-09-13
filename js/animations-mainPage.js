e85b6e432352a8382b4e36d1e2137cf0bb39c77b
Asia <dobrowolska.krakow@gmail.com>
2024-09-13 22:10:30 +0200

    carusel

--- js/animations-mainPage.js
+++ js/animations-mainPage.js
@@ -20,3 +20,27 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.carousel', {
+    loop: true,
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    speed: 3000,
+    centeredSlides: true,
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+    },
+    freeMode: {
+      enabled: true,
+      momentum: false,
+    },
+    grabCursor: true,
+    slideClass: 'carousel__item',
+    wrapperClass: 'carousel__track',
+  });
+  swiper.el.addEventListener('click', () => {
+    swiper.autoplay.start(); // Wznawia autoplay po kliknięciu
+  });
+});
