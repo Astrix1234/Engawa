fea457b2eafab7b1ec8414390f07a045699e370f
Asia <dobrowolska.krakow@gmail.com>
2024-09-15 20:03:49 +0200

    mobile nav correction

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -10,10 +10,11 @@
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    if (!isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
   };
 
   mobileMenu.addEventListener('click', toggleMenu);
@@ -23,6 +24,6 @@
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
+    document.body.style.overflow = '';
   });
 })();
