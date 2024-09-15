4947c0c0a50d609cbcd4a23286827de68966bf95
Asia <dobrowolska.krakow@gmail.com>
2024-09-15 19:45:03 +0200

    mobile nav

---
+++ js/links-active-mobile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const sections = document.querySelectorAll('section');
+  const navLinks = document.querySelectorAll('.mobile-nav__link');
+  const logo = document.querySelector('.mobil-menu__logo');
+
+  function setActiveLink() {
+    let index = sections.length;
+
+    while (--index && window.scrollY + 150 < sections[index].offsetTop) {}
+
+    navLinks.forEach(link => link.classList.remove('active'));
+
+    if (index > 0) {
+      navLinks[index - 1].classList.add('active');
+    }
+
+    if (index === 0) {
+      logo.classList.add('active');
+    } else {
+      logo.classList.remove('active');
+    }
+  }
+
+  setActiveLink();
+  window.addEventListener('scroll', setActiveLink); //
+});
